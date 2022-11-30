57a54ef883e7c7fec65c592ac611384e7cc87b22
sammy-yuntin <yuntinsammy@yahoo.com>
2022-11-30 09:27:12 +0100

    first commit on the project with just cards

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React  from "react";
+import Todo from "./components/Todo";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MY Todos</h1>
+      <Todo/> 
+      <Todo/> 
+      <Todo/> 
+    </div>
+  );
+}
+
+export default App;
+ 
